b11200759fe6435380451e1fd20a7834a4f7eac6
Wind-net <yves@wlankabel.ch>
2024-12-04 15:19:23 +0100

    fix: final fixed for prefix switch

--- main.tf
+++ main.tf
@@ -7,13 +7,10 @@ locals {
 
   worker_instances_map     = zipmap(local.worker_instances_list, local.worker_instances_list)
   controller_instances_map = zipmap(local.controller_instances_list, local.controller_instances_list)
-}
 
-data "template_file" "cloud_init_k0s" {
-  template = file("${path.module}/cloud_init_k0s.yaml.tpl")
-  vars = {
+  cloud_init_k0s = templatefile("${path.module}/cloud_init_k0s.yaml.tftpl", {
     hostname_prefix = var.hostname_prefix
-  }
+  })
 }
 
 # This Host is used to provision the k0s cluster
@@ -24,7 +21,7 @@ resource "maas_vm_instance" "k8s_controller_init" {
   storage   = 30
   hostname  = "${var.hostname_prefix}-c1"
   zone      = "10-3-24-0"
-  user_data = data.template_file.cloud_init_k0s.rendered
+  user_data = local.cloud_init_k0s
 
   depends_on = [maas_vm_instance.k8s_controller, maas_vm_instance.k8s_worker]
 }
@@ -38,7 +35,6 @@ resource "maas_vm_instance" "k8s_controller" {
   storage   = 30
   hostname  = "${var.hostname_prefix}-c${each.key}"
   zone      = "10-3-24-0"
-  user_data = data.template_file.cloud_init_k0s.rendered
 }
 
 # k0s worker instances
@@ -50,5 +46,4 @@ resource "maas_vm_instance" "k8s_worker" {
   storage   = 30
   hostname  = "${var.hostname_prefix}-w${each.key}"
   zone      = "10-3-24-0"
-  user_data = data.template_file.cloud_init_k0s.rendered
 }
